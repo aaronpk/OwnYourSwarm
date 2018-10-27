9090368d9073c3d94e9d5ad73844315ffb56f5b2
Aaron Parecki <aaron@parecki.com>
2018-10-27 10:41:18 -0700

    stop making bad requests

--- scripts/backfeed.php
+++ scripts/backfeed.php
@@ -4,6 +4,9 @@ include('vendor/autoload.php');
 
 $users = ORM::for_table('users')
   ->where_lt('date_next_poll', date('Y-m-d H:i:s'))
+  ->where('micropub_success', 1)
+  ->where_not_equal('foursquare_access_token', '')
+  ->where_not_equal('micropub_access_token', '')
   ->find_many();
 foreach($users as $user) {
   Backfeed::run($user->id);
